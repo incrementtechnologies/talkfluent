ef402c609ef37e7a9d9d67729f307f8f4864dc84
GarridoL <lalainecabelin@gmail.com>
2021-06-10 08:41:21 +0800

    updates on plan and dashboard

--- src/modules/dashboard/dashboard.vue
+++ src/modules/dashboard/dashboard.vue
@@ -46,12 +46,9 @@
           <div style="margin-top: 80px; margin-bottom: 20px">
             <div class="wistia_responsive_padding" style="padding:49.06% 0 0 0;position:relative;">
               <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-                <div class="wistia_embed wistia_async_ud7le43chh videoFoam=true" style="height:100%;position:relative;width:100%">
-                  <div class="wistia_swatch"
-                    style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;">
-                    <img src="https://fast.wistia.com/embed/medias/ud7le43chh/swatch"
-                      style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true"
-                      onload="this.parentNode.style.opacity=1;" />
+                <div class="wistia_embed wistia_async_ud7le43chh" style="height:100%;position:relative;width:100%">
+                  <div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;">
+                    <img src="https://fast.wistia.com/embed/medias/ud7le43chh/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
                   </div>
                 </div>
               </div>
