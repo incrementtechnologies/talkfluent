363df86310b903b8d0512ca65602d192973ba325
= <=>
2021-06-20 15:40:23 +0800

    update

--- api/app/Http/Controllers/StripeController.php
+++ api/app/Http/Controllers/StripeController.php
@@ -341,7 +341,7 @@ class StripeController extends TalkController
       // update with prorate, note the prorate will take effect on next billing
       $pk = ($keys['flag'] == false || $keys['flag'] == 'false') ? $keys['stripe']['dev_pk'] : $keys['stripe']['live_pk'];
       $sk = ($keys['flag'] == false  || $keys['flag'] == 'false') ? $keys['stripe']['dev_sk'] : $keys['stripe']['live_sk'];
-      $stripeCard = StripeSubscription::where('account_id', '=', $data['account_id'])->first();
+      $stripeCard = StripeSubscription::where('account_id', '=', $data['account_id'])->orderBy('created_at', 'desc')->first();
       $account = Account::where('id', '=', $data['account_id'])->first();
       $newPlan = $data['plan'];
       if($account && $account->paused_on != null && $newPlan == 'pause'){
